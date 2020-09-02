1dfe207656035ecc4c2d9beec7a93292068ea9cc
wangbinbin <wangbinbin@gzlplink.com>
2020-09-02 09:24:29 +0800

    提交示例代码

---
+++ src/main/java/com/example/feldemo/FelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.feldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelDemoApplication.class, args);
+    }
+
+}
